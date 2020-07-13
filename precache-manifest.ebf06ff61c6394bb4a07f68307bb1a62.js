2bf62b21563e6aa7dbce4f5c91aebd4262988622
Gostyu <cedric.lissossi@gmail.com>
2020-07-13 14:45:42 +0200

    Updates

---
+++ precache-manifest.ebf06ff61c6394bb4a07f68307bb1a62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9007ac7035f5412e4d3f779a60551b99",
+    "url": "/fb_login_screen/index.html"
+  },
+  {
+    "revision": "5ad420b8840ffbec3a8f",
+    "url": "/fb_login_screen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab66626e92600f4c88e7",
+    "url": "/fb_login_screen/static/js/2.2f845c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fb_login_screen/static/js/2.2f845c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad420b8840ffbec3a8f",
+    "url": "/fb_login_screen/static/js/main.37ed188f.chunk.js"
+  },
+  {
+    "revision": "e3966bee490b0622a3bb",
+    "url": "/fb_login_screen/static/js/runtime-main.01f51267.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fb_login_screen/static/media/logo.5d5d9eef.svg"
+  }
+]);
