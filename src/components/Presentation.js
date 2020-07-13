b86f43827ce8261668aecada10bc5884d3e21d1b
Gostyu <cedric.lissossi@gmail.com>
2020-07-14 01:10:00 +0200

    some changes on css files

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Presentation.css";
-import img from "../assets/social_media.svg"
+import {ReactComponent as SocialMediaSVG} from "../assets/social_media.svg"
 export default function Presentation({firstTitle,secondTitle, thirdTitle}) {
     return (<div className="left_container">
         <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
@@ -10,6 +10,6 @@ export default function Presentation({firstTitle,secondTitle, thirdTitle}) {
             <h1>and the world around</h1>
             <h1>you on Facebook</h1>
         </div>
-        <img src={img} alt="" className="left_container--img"/>
+        <SocialMediaSVG className="left_container--img"/>
     </div>);
 };
