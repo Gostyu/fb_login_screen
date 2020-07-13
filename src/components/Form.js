a966a820028702c626e5a16f7868384e8df0a96d
Gostyu <cedric.lissossi@gmail.com>
2020-07-14 01:24:41 +0200

    left-side finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,30 @@ import React, { Fragment } from"react";
 import "./Form.css";
 import PrimaryButton from "./PrimaryButton.js";
 import InputField from "./InputField";
-export default function Form({label}){
+// export default function Form({label}){
 
-    return <Fragment>
-        {(label === "Log In") && (
-            <Fragment>
-                <form class="">
-                <h1>{label}</h1>
-                <InputField placeholder="Email or Phone"></InputField>
-                <InputField placeholder="Password"></InputField>
-            <PrimaryButton title="LogIn"></PrimaryButton>
-            </form>
-         </Fragment>)
-        }
-        {
-            (label==="Sign Up") && (
-                <Fragment>
+//     return <Fragment>
+//         {(label === "Log In") && (
+//             <Fragment>
+//                 <form class="">
+//                 <h1>{label}</h1>
+//                 <InputField placeholder="Email or Phone"></InputField>
+//                 <InputField placeholder="Password"></InputField>
+//             <PrimaryButton title="LogIn"></PrimaryButton>
+//             </form>
+//          </Fragment>)
+//         }
+//         {
+//             (label==="Sign Up") && (
+//                 <Fragment>
                 
-                <h1>{label}</h1>
-                <InputField placeholder="Email or Phone"></InputField>
-                <InputField placeholder="Email or Phone"></InputField>
-            <PrimaryButton title="LogIn"></PrimaryButton>
-         </Fragment>
-            )
-        }
+//                 <h1>{label}</h1>
+//                 <InputField placeholder="Email or Phone"></InputField>
+//                 <InputField placeholder="Email or Phone"></InputField>
+//             <PrimaryButton title="LogIn"></PrimaryButton>
+//          </Fragment>
+//             )
+//         }
          
-    </Fragment>
-}
+//     </Fragment>
+// }
--- src/components/RegisterAndLogIn.js
+++ src/components/RegisterAndLogIn.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./RegisterAndLogIn.css";
 import Form from "./Form";
-export default function RegisterAndLogIn() {
+// export default function RegisterAndLogIn() {
 
-    return (<div className="right_container">
-        <Form label="Log In">
-        </Form>
-        <Form label="Sign Up">
-        </Form>
-    </div>);
-};
+//     return (<div className="right_container">
+//         <Form label="Log In">
+//         </Form>
+//         <Form label="Sign Up">
+//         </Form>
+//     </div>);
+// };
